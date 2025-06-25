cc45365652ea66ea59b0f99d84b498cfb145afa0
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-25 11:14:41 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,175 @@
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+const products = [
+  {
+    id: 1,
+    title: "Air Force",
+    price: 119,
+    colors: [
+      {
+        code: "black",
+        img: "air.png",
+      },
+      {
+        code: "darkblue",
+        img: "air2.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: 149,
+    colors: [
+      {
+        code: "lightgray",
+        img: "jordan.png",
+      },
+      {
+        code: "green",
+        img: "jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: 109,
+    colors: [
+      {
+        code: "lightgray",
+        img: "blazer.png",
+      },
+      {
+        code: "green",
+        img: "blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: 129,
+    colors: [
+      {
+        code: "black",
+        img: "crater.png",
+      },
+      {
+        code: "lightgray",
+        img: "crater2.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Hippie",
+    price: 99,
+    colors: [
+      {
+        code: "gray",
+        img: "hippie.png",
+      },
+      {
+        code: "black",
+        img: "hippie2.png",
+      },
+    ],
+  },
+];
+
+let choosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    //change the current slide
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    //change the choosen product
+    choosenProduct = products[index];
+
+    //change texts of currentProduct
+    currentProductTitle.textContent = choosenProduct.title;
+    currentProductPrice.textContent = "$" + choosenProduct.price;
+    currentProductImg.src = choosenProduct.colors[0].img;
+
+    //assing new colors
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
+  });
+});
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = choosenProduct.colors[index].img;
+  });
+});
+
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
+  });
+});
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
+let col_btn = document.querySelectorAll(".btn-col");
+let col_item = document.querySelectorAll(".collection-item");
+
+col_btn.forEach((btn,index)=>{
+    btn.addEventListener("click",(e)=>{
+        col_btn.forEach((col_bt,ind)=>{
+            col_bt.classList.remove("btn");
+        })
+        e.target.classList.add("btn");
+        let data_btn = btn.getAttribute("data-btn");
+        col_item.forEach((col,inde)=>{
+            if(col.getAttribute("data-item")==data_btn || data_btn == "all"){
+                col.classList.remove("hide")
+            }else{
+                col.classList.add("hide");
+            }
+        })
+    })
+})
+
+
+// nav bar
+
+let ul = document.querySelector("ul");
+let burger_icon = document.querySelector(".burger_icon");
+
+burger_icon.addEventListener("click",()=>{
+    if(burger_icon.classList.contains("fa-bars")){
+        burger_icon.classList.add("fa-xmark");
+        burger_icon.classList.remove("fa-bars");
+        ul.classList.add("active");
+    }
+    else{
+        burger_icon.classList.remove("fa-xmark");
+        burger_icon.classList.add("fa-bars");
+        ul.classList.remove("active");
+    }
+})
